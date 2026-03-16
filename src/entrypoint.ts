c53cbc34efa8423d721b32641e68a4f15961b836
TJ Eastmond <tj.eastmond@gmail.com>
2026-03-16 18:15:53 -0400

    fix: clean up smart order entrypoint output

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,12 +1,18 @@
+import { db } from "./db";
 import triggerSmartOrderSystem from "./smartOrder";
 
 const DEFAULT_SERVICE_DATE = new Date(Date.UTC(2025, 7, 24));
 
-triggerSmartOrderSystem(DEFAULT_SERVICE_DATE)
+void triggerSmartOrderSystem(DEFAULT_SERVICE_DATE)
   .then((summary) => {
-    console.log("Done", summary);
+    const { patientResults: _patientResults, ...loggableSummary } = summary;
+    console.log("\nDone:", loggableSummary);
   })
   .catch((error: Error) => {
+    process.exitCode = 1;
     console.error("Something went wrong");
     console.error(error);
+  })
+  .finally(async () => {
+    await db.$disconnect();
   });
